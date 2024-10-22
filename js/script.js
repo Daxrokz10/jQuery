112a5d9e930305384a36236d3b98ff0617c59a85
Daxrokz10 <dakshgagnani@gmail.com>
2024-10-22 10:52:21 +0530

    hover event

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-$('p').click(function(){
-    console.log('you clicked on p');
-    $('p').hide();
+$(document).ready(function (){
+
+
+    $('p').click(function(){
+        console.log('you clicked on p');
+        $('p').hide();
+    });
+
+    $('p').hover(function(){
+        console.log('you hovered on :' , this);
+    });
+
+
 });
