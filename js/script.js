852d791ab34c54c0ffa64cc4072616468455c691
Daxrokz10 <dakshgagnani@gmail.com>
2024-10-22 11:04:09 +0530

    double function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function (){
 
 
-    $('p').click(function(){
-        console.log('you clicked on p');
-        $('p').hide();
+    // $('p').click(function(){
+    //     console.log('you clicked on p');
+    //     $('p').hide();
+    // });
+
+    // $('p').hover(function(){console.log('you hovered on :' , this);});
+
+    $('p').dblclick(function(){
+        console.log('you double clicked on : ',this);
+        console.log('thanks');
     });
-
-    $('p').hover(function(){
-        console.log('you hovered on :' , this);
-    });
-
-
 });
