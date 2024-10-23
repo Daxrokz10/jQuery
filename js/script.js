2e20359009c3cdea469f740330b13bdf6619bfdb
Daxrokz10 <dakshgagnani@gmail.com>
2024-10-23 12:49:59 +0530

    color changer

--- js/script.js
+++ js/script.js
@@ -38,10 +38,41 @@ $(document).ready(function (){
     //     } 
     // });
 
+    
 
-    $('#btn').click(function(){
-        $('#text').hide(function(){
-            console.log("text has been hidden");
-        })
-    })
-}); 
+
+
+// let textViewer = true
+
+//     $('#btn').click(function(){
+//         if(textViewer) { $('#text').hide(function(){ console.log('text hidden');  textViewer=false; }); } 
+    
+//         else { $('#text').show(function(){ console.log('text shown'); textViewer=true; }); }
+//     });
+
+
+    // const colors = ['lightblue', '#9653ef', 'lightcoral', 'lightgoldenrodyellow', 'lightpink'];
+    // const colorNames = ['Light Blue', 'Purple', 'Light Coral', 'Light Golden Rod Yellow', 'Light Pink'];
+    // let currentColorIndex = 0;
+
+    // $('#colorBtn').click(function () {
+    //     currentColorIndex = (currentColorIndex + 1) % colors.length; // Cycle through colors
+    //     $('#colorBox').css('background-color', colors[currentColorIndex]);
+    //     $('#colorName').text(colorNames[currentColorIndex]); // Update the color name display
+    // });
+
+    const color = ['lightblue', 'black' , 'red' , 'green'];
+    const colorName = ['Light Blue', 'Black', 'Red', 'Green'];
+    let currentColorValue = 0;
+
+    $('#colorBtn').click(function(){
+        currentColorValue = (currentColorValue + 1) % color.length;
+        $('#colorBox').css('background-color', color[currentColorValue]);
+        $('#colorName').text(colorName[currentColorValue]); 
+        console.log('color has been changed');
+    });
+
+
+});
+
+
